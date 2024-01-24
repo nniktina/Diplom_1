d100d8c414c86ad641b94de9ba05e964d3ebdf42
Anastasiya Nikitina <anastasia.nikitina@aliexpress.ru>
2024-01-24 17:58:38 +0300

    two more tests

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -1,5 +1,7 @@
+from praktikum.ingredient import Ingredient
 from tests.mocks import MockData
 from praktikum.burger import Burger
+from praktikum.database import Database
 
 
 class TestBurger:
@@ -16,7 +18,17 @@ class TestBurger:
         burger.add_ingredient(mocked_ingredient)
         assert mocked_ingredient in burger.ingredients
 
+    def test_burger_remove_ingredient(self):
+        burger = Burger()
+        mocked_ingredient = MockData.mock_ingredient()
+        burger.add_ingredient(mocked_ingredient)
+        burger.remove_ingredient(0)
+        assert mocked_ingredient not in burger.ingredients
 
-
-
-
+    def test_burger_move_ingredient(self):
+        burger = Burger()
+        database = Database()
+        burger.add_ingredient(database.ingredients[0])
+        burger.add_ingredient(database.ingredients[3])
+        burger.move_ingredient(0, 1)
+        assert burger.ingredients[1].name == database.ingredients[0].name
