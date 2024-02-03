71d881e79e2fda9b2cce63e87369853cfa6489bf
Anastasiya Nikitina <anastasia.nikitina@aliexpress.ru>
2024-02-03 09:43:45 +0300

    fix data

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -12,7 +12,7 @@ class TestBun:
         assert result_name == bun_name
 
     @pytest.mark.parametrize("bun_name, bun_price", data.bun_test_data)
-    def test_bun_get_name(self, bun_name, bun_price):
+    def test_bun_get_price(self, bun_name, bun_price):
         bun = Bun(bun_name, bun_price)
-        result_name = bun.get_price()
-        assert result_name == bun_price
+        result_price = bun.get_price()
+        assert result_price == bun_price
